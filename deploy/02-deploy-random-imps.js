59d2f9bc3132a068d90fd2983ac2db093cefc599
priyanshu-pj <homepc20140422@outlook.com>
2022-08-02 15:24:16 +0530

    fund VRFCoordinatorV2Mock

--- deploy/02-deploy-random-imps.js
+++ deploy/02-deploy-random-imps.js
@@ -17,6 +17,8 @@ const metadataTemplate = {
     ]
 }
 
+const FUND_AMOUNT = "1000000000000000000000"
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -38,6 +40,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         const tx = await VRFCoordinatorV2Mock.createSubscription()
         const txReceipt = await tx.wait(1)
         subscriptionId = txReceipt.events[0].args.subId
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
         subscriptionId = networkConfig[chainId].subscriptionId
